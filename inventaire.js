811a876ce54263f64de933056cbc9adf4d93616f
Florent GENIET <florent.geniet@laposte.net>
2018-11-21 01:08:36 +0100

    déploiment de l'inventaire version test

--- inventaire.js
+++ inventaire.js
@@ -1,7 +1,23 @@
-var inventaire = document.getElementById("Inventaire");
+var inventaire = document.getElementById("bouttonInventaire");
 var objet = document.getElementById("Objets");
 inventaire.addEventListener('click', valider);
 
-function valider (){
-    
+var objetsAccessibles = [{nom : "clés", image : "images/cles.jpg"}];
+var ouvert = false;
+var cle = document.createElement("div");
+cle.id = "clé";
+cle.style.backgroundImage = "url('images/cles.jpg')";
+cle.style.backgroundSize = "cover";
+cle.style.height = "100px";
+cle.style.width = "100px";
+
+
+function valider (event){
+    if (ouvert){
+        objet.removeChild(cle);
+    }
+    else{
+        objet.appendChild(cle);
+    }
+    ouvert = !ouvert;
 }
