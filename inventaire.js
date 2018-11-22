e23284f3ef9e14e846e7c72e4c41bbff5d3afc44
Florent GENIET <florent.geniet@laposte.net>
2018-11-22 11:20:00 +0100

    modif de l'interface

--- inventaire.js
+++ inventaire.js
@@ -1,10 +1,13 @@
 var inventaire = document.getElementById("bouttonInventaire");
+var inv = document.getElementById("Inventaire");
 var objet = document.getElementById("Objets");
 inventaire.addEventListener('click', valider);
 
 var objetsAccessibles = [{nom : "clés", image : "images/cles.jpg"}];
 var ouvert = false;
 var cle = document.createElement("div");
+objet.appendChild(cle);
+
 cle.id = "clé";
 cle.style.backgroundImage = "url('images/cles.png')";
 cle.style.backgroundSize = "100% 100%";
@@ -12,6 +15,7 @@ cle.style.height = "78px";
 cle.style.width = "78px";
 
 var reveil = document.createElement("div");
+objet.appendChild(reveil);
 reveil.id = "réveil";
 reveil.style.backgroundImage = "url('images/alarme.png')";
 reveil.style.backgroundSize = "100% 100%";
@@ -21,12 +25,10 @@ reveil.style.width = "78px";
 
 function valider (event){
     if (ouvert){
-        objet.removeChild(cle);
-        objet.removeChild(reveil);
+        inv.removeChild(objet);
     }
     else{
-        objet.appendChild(cle);
-        objet.appendChild(reveil);
+        inv.appendChild(objet);
     }
     ouvert = !ouvert;
 }
