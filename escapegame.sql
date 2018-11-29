e30b0cbe5f5671f098f7c18cdaf4107906a18314
Ardamas <MaxParant@gmail.com>
2018-11-29 10:18:32 +0100

    Màj Bdd

--- escapegame.sql
+++ escapegame.sql
@@ -2,10 +2,10 @@
 -- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  mer. 28 nov. 2018 à 17:13
--- Version du serveur :  5.7.17
--- Version de PHP :  5.6.30
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2018 at 10:16 AM
+-- Server version: 5.7.17
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `escapegame`
+-- Database: `escapegame`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `objectif`
+-- Table structure for table `objectif`
 --
 
 CREATE TABLE `objectif` (
@@ -42,17 +42,19 @@ CREATE TABLE `objectif` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `objectif`
+-- Dumping data for table `objectif`
 --
 
 INSERT INTO `objectif` (`id`, `intitule`, `objectif_suivant`, `objet1`, `objet2`, `evenement_de_reussite`, `victoire`, `gain`, `debut`, `bonus`) VALUES
-(1, 'Cliquez sur le réveil pour réveiller la coloc', 2, 1, -1, 'click', 0, 0.1, 1, 0),
-(2, '- Trouvez la clé de la coloc\' pour fermer la maison avant de partir.', 3, 2, 3, 'superposition', 0, 0, 0, 0);
+(1, '- Cliquez sur le réveil pour réveiller la coloc', 2, 1, -1, 'click', 0, 0.1, 1, 0),
+(2, '- Trouvez la clé de la coloc\' pour fermer la maison avant de partir.', 3, 2, 3, 'superposition', 0, 0, 0, 0),
+(3, '- Prenez la voiture au parking.', 4, 4, -1, 'click', 0, 0.1, 0, 0),
+(4, '- Rejoignez le centre IGN à Forcalquier.', 5, 5, -1, 'click', 0, 0.1, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `objet`
+-- Table structure for table `objet`
 --
 
 CREATE TABLE `objet` (
@@ -63,23 +65,24 @@ CREATE TABLE `objet` (
   `lon` double NOT NULL,
   `descriptif` varchar(300) NOT NULL,
   `indice` varchar(300) NOT NULL,
-  `zoom_affichage` int(11) NOT NULL,
-  `objet_debloque` int(11) NOT NULL
+  `zoom_affichage` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `objet`
+-- Dumping data for table `objet`
 --
 
-INSERT INTO `objet` (`id`, `name`, `image`, `lat`, `lon`, `descriptif`, `indice`, `zoom_affichage`, `objet_debloque`) VALUES
-(1, 'reveil', 'images/alarme.png', 44.0085, 5.79356, 'Le reveil de la coloque, il a le pouvoir de reveiller quiconque l\'entend...pour mieux se rendormir ensuite.....', 'Clique sur le réveil pour reveiller la coloque', 22, 2),
-(2, 'cle', 'images/cle.png', 44.00848, 5.7936, 'Les clés de la coloc, utile pour ne pas se faire voler, ou disputer par Marie.', 'Les clés servent souvent à fermer et ouvrir des choses, comme une porte par exemple.', 22, 3),
-(3, 'porte', 'images/door_open.png', 44.00848, 5.79357, 'UNE PORTE!!! ON PEUT FAIRE DES CHOOSES GROS!!!', '', 0, 4);
+INSERT INTO `objet` (`id`, `name`, `image`, `lat`, `lon`, `descriptif`, `indice`, `zoom_affichage`) VALUES
+(1, 'reveil', 'images/alarme.png', 44.0085, 5.79356, 'Le reveil de la coloque, il a le pouvoir de reveiller quiconque l\'entend...pour mieux se rendormir ensuite.....', 'Clique sur le réveil pour reveiller la coloque', 18),
+(2, 'cle', 'images/cle.png', 44.00848, 5.7936, 'Les clés de la coloc, utile pour ne pas se faire voler, ou disputer par Marie.', 'Les clés servent souvent à fermer et ouvrir des choses, comme une porte par exemple.', 22),
+(3, 'porte', 'images/door_open.png', 44.00848, 5.79357, 'UNE PORTE!!! ON PEUT FAIRE DES CHOOSES GROS!!!', 'Voilà une porte qui a besoin d\'une clé pour être fermée...sinon Marie va gueuler.', 0),
+(4, 'voiture', 'images/voiture.png', 44.00803, 5.79414, 'Le trajet dans celle de Marie coûte exactement 0.25 centimes. Sinon y\'a celle de Florent, c\'est gratuit et ça va plus vite!', 'Le parking est au sud de la coloc\'.', 0),
+(5, 'Portail', 'images/portail.png', 43.96202, 5.77388, 'Le portail est fermé on dirait. Vous êtes en avance. Seul pageot pourrait vous aider.', 'Je vous ouvre ce portail, vous en faites pas!', 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `objet_a_debloquer`
+-- Table structure for table `objet_a_debloquer`
 --
 
 CREATE TABLE `objet_a_debloquer` (
@@ -88,25 +91,27 @@ CREATE TABLE `objet_a_debloquer` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `objet_a_debloquer`
+-- Dumping data for table `objet_a_debloquer`
 --
 
 INSERT INTO `objet_a_debloquer` (`id_objectif`, `id_objet`) VALUES
 (1, 2),
-(1, 3);
+(1, 3),
+(2, 4),
+(3, 5);
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `objectif`
+-- Indexes for table `objectif`
 --
 ALTER TABLE `objectif`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `objet`
+-- Indexes for table `objet`
 --
 ALTER TABLE `objet`
   ADD PRIMARY KEY (`id`);
